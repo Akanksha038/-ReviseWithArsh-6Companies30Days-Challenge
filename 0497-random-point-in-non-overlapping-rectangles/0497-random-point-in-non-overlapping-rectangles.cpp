cceb9ec908f915802a87a4c7e3754bfc5a7c3bb8
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-02-07 23:14:50 +0530

    Update 0497-random-point-in-non-overlapping-rectangles.cpp

--- 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
@@ -1,21 +1,3 @@
-// class Solution {
-// public:
-//     Solution(vector<vector<int>>& rects) {
-        
-//     }
-    
-//     vector<int> pick() {
-        
-//     }
-// };
-
-// /**
-//  * Your Solution object will be instantiated and called as such:
-//  * Solution* obj = new Solution(rects);
-//  * vector<int> param_1 = obj->pick();
-//  */
-
-
 class Solution {
  public:
   Solution(vector<vector<int>>& rects) : rects(move(rects)) {
