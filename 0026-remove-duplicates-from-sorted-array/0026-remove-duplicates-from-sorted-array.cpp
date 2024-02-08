ae79c0a8b2339ac23dcd4a7341e45d565c27d42c
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-02-08 12:44:18 +0530

    Time: 4 ms (88.16%), Space: 21.9 MB (5.08%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=nums.size();
-        set < int > set;
-  for (int i = 0; i < n; i++) {
+    int n=nums.size();
+    set < int > set;
+    for (int i = 0; i < n; i++)
+    {
     set.insert(nums[i]);
-  }
-  int k = set.size();
-  int j = 0;
-  for (int x: set) 
-  {
-    nums[j++] = x;
-  }
-  for (int i = 0; i < k; i++) {
-    cout << nums[i] << " ";
-  }
-  for(int j=set.size()+1;j<=n;j++){
-      cout<<"_"<<" ";
-  }
+    }
+     int k = set.size();
+     int j = 0;
+      for (int x: set) 
+      {
+       nums[j++] = x;
+       }
+    //  for (int i = 0; i < k; i++) 
+    // {
+    // cout << nums[i] << " ";
+    // }
+  // for(int j=set.size()+1;j<=n;j++)
+  // {
+  //     cout<<"_"<<" ";
+  // }
 
   return k;
         
